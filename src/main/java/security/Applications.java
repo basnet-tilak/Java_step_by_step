a13733616050a8bdc6bcbf89cde88d0bc1ffed2a
Tilak Basnet <tilak.basnet@outlook.com>
2024-03-29 19:12:34 +0100

    First step of the spring boot application run successfully

--- src/main/java/security/Applications.java
+++ src/main/java/security/Applications.java
@@ -1,7 +1,11 @@
 package security;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Applications {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(Applications.class,args);
     }
 }
