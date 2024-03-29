5d48e3cde649b575bacefed8104148599b40b948
Tilak Basnet <tilak.basnet@outlook.com>
2024-03-29 19:53:05 +0100

    Has been added the application.properties file for database and updated Application for CommandLineRunner

--- src/main/java/security/Applications.java
+++
@@ -1,7 +0,0 @@
-package security;
-
-public class Applications {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/main/java/step_by_step/Applications.java
+++ src/main/java/step_by_step/Applications.java
@@ -1,11 +1,22 @@
 package step_by_step;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class Applications {
+public class Applications implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(Applications.class,args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter the your Name: ");
+        String name = obj.next();
+        System.out.println(name);
     }
 }
