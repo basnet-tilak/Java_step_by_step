ff2c95825c1e28c903c20b32c805dbd169ddb4f2
Tilak Basnet <tilak.basnet@outlook.com>
2024-03-29 20:25:13 +0100

    Has been added the Added the annotation of Reposiory and Service

--- src/main/java/step_by_step/UsersRepository/UsersRepository.java
+++ src/main/java/step_by_step/UsersRepository/UsersRepository.java
@@ -1,8 +1,10 @@
 package step_by_step.UsersRepository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import step_by_step.model.Users;
 
+@Repository
 public interface UsersRepository  extends JpaRepository<Users,Long> {
 
 }
---
+++ src/main/java/step_by_step/services/UserServices.java
@@ -0,0 +1,7 @@
+package step_by_step.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServices {
+}
