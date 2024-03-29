7b3afb78224c1ac265e22d8301bf351a113895df
Tilak Basnet <tilak.basnet@outlook.com>
2024-03-29 19:12:34 +0100

    First step of the spring boot application run successfully

---
+++ src/main/java/step_by_step/Applications.java
@@ -0,0 +1,11 @@
+package step_by_step;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Applications {
+    public static void main(String[] args) {
+        SpringApplication.run(Applications.class,args);
+    }
+}
